502af2f98544c25acde4f8a765283d22d7a2f0ed
0707yiliu <0707yi.liu@gmail.com>
2022-08-17 11:41:22 +0200

    ur3 xml file

---
+++ envs/ur3_gripper_box.py
@@ -0,0 +1,13 @@
+from mujoco_py import load_model_from_path, MjSim
+import mujoco_py as mj
+
+import math
+import random
+import time
+import numpy as np
+import cv2
+import logging
+import gym
+import matplotlib as mpl
+import os
+
